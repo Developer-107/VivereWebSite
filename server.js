051c9f7720a0e53c14319dc1cb58266fbae8b93b
Developer-107 <somemail514@gmail.com>
2026-03-01 16:29:16 +0400

    Update catch-all route for React SPA routing

--- server.js
+++ server.js
@@ -9,8 +9,8 @@ const __dirname = path.dirname(__filename);
 // Serve static files from the build
 app.use(express.static(path.join(__dirname, "dist")));
 
-// Handle React SPA routing
-app.get("*", (req, res) => {
+// Catch-all route for React SPA
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "dist", "index.html"));
 });
 
